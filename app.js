f9fdf17d397e16a8b7dfe2328f029b9081cb21a8
Danial Keoki <d.keoki@gmail.com>
2021-01-17 02:42:56 +0900

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('<h1>Update Test</h1><p>Bye bye manual updates</p>'));
+app.get('/', (req, res) => res.send('<h1>Update Test</h1><p>Bye bye manual updates</p><p>Test manual approval</p>'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
